d668d2d2098a2e6971f52dff4b3ffb75b23eff7c
Rizky Dharma <rizkydarma536@gmail.com>
2024-07-08 15:28:35 +0700

    fixing bug

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -17,7 +17,7 @@ class Mahasiswa extends Controller{
     }
     public function tambah(){
         if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0){
-            header('Location: ' . BASEURL . '/mahasiswa/index');
+            header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
     }
